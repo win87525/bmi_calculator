0b4e29f7883e10e21de5a5fc2f54b2987488158b
緋 <win87525@gmail.com>
2024-04-22 01:01:07 +0800

    forntend_mentor

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+// import { RouterLink, RouterView } from 'vue-router'
+import HomeView from './views/HomeView.vue';
+</script>
+
+<template>
+  <HomeView></HomeView>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <p>我是首頁</p>
+  </main>
+</template>
