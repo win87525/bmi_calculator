7a439f4d10c1e8f1e4b4f192859b6498d37ee760
RS02\rs02 <win87525@gmail.com>
2024-04-26 19:15:13 +0800

    0426

--- src/component/hero/hero.vue
+++ src/component/hero/hero.vue
@@ -1,16 +1,19 @@
 <template>
-    <div class="hero">
-        <p>123</p>
-    </div>
+    <section>
+        <div>
+            <h1>Body Mass
+                Index Calculator
+            </h1>
+            <p>Better understand your weight in relation to your height using our body mass index (BM) calculator. While BMI is not the sole determinant of a healthy weight, it offers a valuable starting point to evaluate your overall health and well-being.</p>
+        </div>
+    </section>
 </template>
 
 
 <style scoped lang="scss">
 @import url('../../assets/css/base.css');
 
-.hero {
-    width: 1276px;
-    height: 737px;
-    background: #000;
+section {
+    margin-left: 2.4rem;
 }
 </style>
