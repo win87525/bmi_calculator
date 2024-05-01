28f02fcc6ac4212750b2f45ab8e9aefecd4561b1
緋 <win87525@gmail.com>
2024-05-02 01:34:53 +0800

    0501

--- src/component/hero/hero.vue
+++ src/component/hero/hero.vue
@@ -1,19 +1,34 @@
 <template>
     <section>
         <div>
-            <h1>Body Mass
-                Index Calculator
-            </h1>
-            <p>Better understand your weight in relation to your height using our body mass index (BM) calculator. While BMI is not the sole determinant of a healthy weight, it offers a valuable starting point to evaluate your overall health and well-being.</p>
+            <div class="titleBox">
+                <h1>Body Mass
+                    Index Calculator
+                </h1>
+                <p>Better understand your weight in relation to your height using our body mass index (BM) calculator.
+                    While BMI is not the sole determinant of a healthy weight, it offers a valuable starting point to
+                    evaluate your overall health and well-being.</p>
+            </div>
+            <div class="bmiBox">
+                <h3>Enter your details below</h3>
+                <div>
+                    <input type="radio" id="Metric" name="switchBtn">
+                    <label for="Metric">Metric</label>
+                    <div>
+                        123456
+                    </div>
+                    <input type="radio" id="Imperial" name="switchBtn">
+                    <label for="Imperial">Imperial</label>
+                </div>
+            </div>
+            <figure>
+                <img src="../../assets/img/logo.svg" alt="">
+            </figure>
         </div>
     </section>
 </template>
 
 
 <style scoped lang="scss">
-@import url('../../assets/css/base.css');
-
-section {
-    margin-left: 2.4rem;
-}
+@import '../../assets/scss/hero.scss';
 </style>
